7e46b8f68af99b4181cfd45ada2d732d760451f5
pf-coding <peterfarkas.coding@gmail.com>
2024-10-06 21:00:45 +0200

    qucikfix

--- src/app/main/order/order.component.ts
+++ src/app/main/order/order.component.ts
@@ -359,7 +359,7 @@ export class OrderComponent implements OnInit {
     // E-mail küldése az összes tételről
     const emailData = {
       to: `bagettos@gmail.com, ${this.orderForm.value.email}`,
-      subject: `Mókusch rendelés ${this.orderForm.value.name} ${this.orderForm.value.date} ${this.orderForm.value.time}`,
+      subject: `Mókusch rendelés: ${this.orderForm.value.name} ${this.orderForm.value.date} ${this.orderForm.value.time}`,
       body: {
         orderForm: this.orderForm.value,
         orderDetails: orderDetails,
